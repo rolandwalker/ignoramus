3ca24d0f4c4b7b1fa17b70fac710fa9b542067a2
Roland Walker <walker@pobox.com>
2012-09-25 13:47:19 -0400

    add tests

--- ert-tests/ignoramus-test.el
+++ ert-tests/ignoramus-test.el
@@ -1,9 +1,62 @@
 
-;; requires and setup
+;;; requires and setup
 
 (require 'ignoramus)
 
-;;; this is a stub - no tests defined
+;;; ignoramus-boring-p
+
+(ert-deftest ignoramus-boring-p-01 nil
+  (should
+   (ignoramus-boring-p "filename~")))
+
+
+;;; ignoramus-compute-common-regexps
+
+(ert-deftest ignoramus-compute-common-regexps-01 nil
+  (should (equal "\\`[abc]\\|\\`[ghi]\\'\\|j\\|k\\|l"
+                 (let ((ignoramus-file-beginnings  '("a" "b" "c"))
+                       (ignoramus-file-endings     '("d" "e" "f"))
+                       (ignoramus-file-exact-names '("g" "h" "i"))
+                       (ignoramus-file-regexps     '("j" "k" "l")))
+                   (ignoramus-compute-common-regexps)
+                   (prog1
+                       ignoramus-boring-dir-regexp
+                     (ignoramus-compute-common-regexps))))))
+
+(ert-deftest ignoramus-compute-common-regexps-02 nil
+   (should (equal "\\`[abc]\\|[def]\\'\\|\\`[ghi]\\'\\|j\\|k\\|l"
+                  (let ((ignoramus-file-beginnings  '("a" "b" "c"))
+                        (ignoramus-file-endings     '("d" "e" "f"))
+                        (ignoramus-file-exact-names '("g" "h" "i"))
+                        (ignoramus-file-regexps     '("j" "k" "l")))
+                    (ignoramus-compute-common-regexps)
+                    (prog1
+                        ignoramus-boring-file-regexp
+                      (ignoramus-compute-common-regexps))))))
+
+;;; ignoramus-setup
+
+(ert-deftest ignoramus-setup-01 nil
+  (should
+   (let ((ignoramus-file-exact-names '("g" "h" "i"))
+         (vc-directory-exclusion-list nil))
+     (ignoramus-setup 'vc)
+     vc-directory-exclusion-list
+     )))
+
+;;; ignoramus-known-actions
+
+(ert-deftest ignoramus-known-actions-01 nil
+  (should
+   (eq (length ignoramus-known-actions)
+       (let ((count 1))         ; 'all counts for 1
+         (save-match-data
+           (mapatoms #'(lambda (sym)
+                         (when (and (functionp sym)
+                                    (string-match "\\`ignoramus-do-ignore-\\(.+\\)" (symbol-name sym))
+                                    (memq (intern (match-string 1 (symbol-name sym))) ignoramus-known-actions))
+                           (incf count)))))
+         count))))
 
 ;;
 ;; Emacs
